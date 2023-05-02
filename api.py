eb5a044015e73ccdeb9438be4f4f3ef1b6af30b8
measic <martin.measic@outlook.com>
2023-05-03 00:56:54 +1000

    ok done for tofay

--- api.py
+++ api.py
@@ -2,17 +2,22 @@ import openai
 from prompts import *
 openai.api_key = "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"
 
+
 def get_response():
     completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    temperature=0,
-    messages=[
-        {"role": "user", "content": "Suggest a new variable name for variables ['a, b, data'] \
-         CONTEXT: {im analyzing rainfall data} \
-         CODE: {data = pd.read_csv('data.csv')\ndata.columns = ['sun', 'mm']\na = data['sun'].mean()\nb = data['mm'].max()} \
-         Output format: [list of tuples of variables and their new names]"},
-    ]
+        model="gpt-3.5-turbo",
+        temperature=0,
+        messages=[
+            {"role": "user", "content": "Task: Rename the variable below to a more descriptive name that reflects \
+         the purpose of the variable, based on the project description and variable usage."},
+            {"role": "user",
+             "content": "Code: 'data = pd.read_csv('data.csv')\ndata.columns = ['sun', 'mm']\na = data['sun'].mean()\nb = data['mm'].max()'"},
+            {"role": "user", "content": "Variable: 'data'"},
+            {"role": "user", "content": "Project description: 'analyzing rainfall'"},
+            {"role": "user", "content": "Output the variable name and a one sentence explanation"},
+        ]
     )
     return completion.choices[0].message
+
 
 print(get_response())
