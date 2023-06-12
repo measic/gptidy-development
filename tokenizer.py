a5aa12770c3e645e8c5822fa61f4b1006be8c5fd
measic <martin.measic@outlook.com>
2023-06-12 20:37:20 +1000

    d

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,5 @@
-import json
 import tiktoken
 import requests
-import re
 
 TOKEN_LIMIT = 4096
 
@@ -17,17 +15,31 @@ def get_cell_list(notebook):
     # get all cells but only keep cell_type and source
     cells = []
     for i in range(len(notebook['cells'])):
-        # if code cell is a list of strings, join them
-        notebook['cells'][i]['source'] = ''.join(notebook['cells'][i]['source'])
+        cell =  notebook['cells'][i]
+        # if source is a list of strings, join them
+        cell['source'] = ''.join(cell['source'])
+
+        if cell['cell_type'] == 'code' and cell['outputs']:
+            # i think the length is always 1?
+            assert len(cell['outputs']) == 1
+            # if output is a list of strings, join them
+            cell['outputs'][0]['data']['text/plain'] = ''.join(cell['outputs'][0]['data']['text/plain'])
+
         cells.append({
-            'type': notebook['cells'][i]['cell_type'],
-            'src': notebook['cells'][i]['source']
+            'id': i,
+            'type': cell['cell_type'],
+            'src': cell['source'],
+            'outputs': cell['outputs'][0]['data']['text/plain'] if cell['cell_type'] == 'code' and cell['outputs'] else None
         })
     return cells
 
 # take cells within subset of token limit
 # not exact but a good approximation
 def get_subset_cells(cells, token_limit):
+    # filter out id and outputs in cells
+    cells = [{'type': cell['type'], 'src': cell['src']} for cell in cells]
+
+    # get content within token limit
     if num_tokens_from_string(str(cells)) <= token_limit:
         return cells
     else:
@@ -48,8 +60,7 @@ def get_subset_cells(cells, token_limit):
             })
         return subset_cells
 
-def get_notebook_contents_summary(url, token_limit=TOKEN_LIMIT // 2):
-    notebook = fetch_notebook(url)
+def get_notebook_contents_summary(notebook, token_limit=TOKEN_LIMIT // 2):
     cells = get_cell_list(notebook)
     subset_cells = get_subset_cells(cells, token_limit)
     return str(subset_cells)
@@ -116,11 +127,27 @@ def get_project_info(url):
     # if project_info contains any info, return it
     return project_info or None
 
-def generate_introduction(url):
+def introduction_prompt(url):
+    notebook = fetch_notebook(url)
+
     project_info = get_project_info(url)
-    cell_contents = get_notebook_contents_summary(url)
+    project_info_tokens = num_tokens_from_string(str(project_info))
+    cell_contents = get_notebook_contents_summary(notebook, TOKEN_LIMIT - project_info_tokens)
     prompt = f"Write a concise 3-5 sentence introduction in markdown which gives an overview of this project's purpose, goals, and structure of topics. The context below provides background information and the cell contents is the notebook itself. Context: {project_info}. Cell contents: {cell_contents}"
     return prompt
 
+def cell_process_prompt(url, cell_id):
+    notebook = fetch_notebook(url)
+    cells = get_cell_list(notebook)
+    prompt = "Write a short 3-5 sentence description of what this code is doing in markdown. Cell contents: " + str({cells[cell_id]['src']})
+    return prompt
+
+def cell_reason_prompt(url, cell_id):
+    notebook = fetch_notebook(url)
+    cells = get_cell_list(notebook)
+    prompt = "Write a short 3-5 sentence description explaining the output of this cell. Cell contents: " + str(cells[cell_id])
+    return prompt
+    
 url = "https://raw.githubusercontent.com/ZaraYar/Analyzing-NYC-High-School-Data/f7de631936b9614ceb38f398327bc6695788970f~1/Basics.ipynb"
-print(generate_introduction(url))
+
+print(cell_reason_prompt(url, 0))
